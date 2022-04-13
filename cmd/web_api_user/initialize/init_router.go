0b9528649147cf162c08250cf3949802d275eca8
ljtian <tianlijun315@gmail.com>
2022-04-13 12:08:41 +0800

    1、添加用户API服务 2、修改注册中心函数入参

---
+++ cmd/web_api_user/initialize/init_router.go
@@ -0,0 +1,30 @@
+package initialize
+
+import (
+	"SESS/cmd/web_api_user/middlewares"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouters(port int) {
+	Router := gin.Default()
+	Router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    http.StatusOK,
+			"success": true,
+		})
+	})
+
+	//配置跨域
+	Router.Use(middlewares.Cors())
+
+	_ = Router.Group("/u/v1")
+	//router.InitUserRouter(ApiGroup)
+	//router.InitBaseRouter(ApiGroup)
+
+	go func() {
+		Router.Run(fmt.Sprintf(":%d", port))
+	}()
+}
